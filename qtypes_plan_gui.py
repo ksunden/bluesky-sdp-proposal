d4366bdc2eb93791ec6d5b2229e40fdb9bf56bc5
Kyle Sunden <git@ksunden.space>
2022-03-30 17:05:41 -0500

    Named types: enums and namedtuple

--- qtypes_plan_gui.py
+++ qtypes_plan_gui.py
@@ -1,4 +1,5 @@
 import ast
+import enum
 import sys
 
 from ophyd import sim
@@ -10,11 +11,17 @@ from bluesky.plans import count, grid_scan
 import qtypes
 from qtpy import QtWidgets
 
-def many_different_params(i: int, f: float, b: bool, s: str, r: protocols.Readable, m: protocols.Movable, fl: protocols.Flyable):
+
+class RGB(enum.Enum):
+    RED=1
+    GREEN=2
+    BLUE=3
+
+def many_different_params(i: int, f: float, b: bool, s: str, r: protocols.Readable, m: protocols.Movable, fl: protocols.Flyable, e: RGB):
     yield bluesky.Msg("null")
 
 
-plans = [count, grid_scan, many_different_params]
+plans = [many_different_params, count, grid_scan]
 
 ser = {x.__name__: serialize_plan(x) for x in plans}
 
@@ -24,12 +31,13 @@ flyables = {k: v for k, v in sim.__dict__.items() if isinstance(v, protocols.Fly
 
 
 class Sequence(qtypes.Null):
-    def __init__(self, name, type_, parent, at=None):
+    def __init__(self, name, type_, parent, named_types, at=None):
         super().__init__(name)
         if at is None:
             parent.append(self)
         else:
             parent.insert(at, self)
+        self.named_types = named_types
         self.type_ = type_
         self.add = qtypes.Button("add")
         self.add.updated.connect(self.on_add)
@@ -39,7 +47,7 @@ class Sequence(qtypes.Null):
         self.append(self.remove)
     
     def on_add(self):
-        get_qtypes_widget(self.type_, "item", self, -2)
+        get_qtypes_widget(self.type_, "item", self, self.named_types, -2)
 
     def on_remove(self):
         try:
@@ -48,12 +56,13 @@ class Sequence(qtypes.Null):
             pass  # Nothing to remove
 
 class Mapping(qtypes.Null):
-    def __init__(self, name, key_type, value_type, parent, at=None):
+    def __init__(self, name, key_type, value_type, parent, named_types, at=None):
         super().__init__(name)
         if at is None:
             parent.append(self)
         else:
             parent.insert(at, self)
+        self.named_types = named_types
         self.key_type = key_type
         self.value_type = value_type
         self.add = qtypes.Button("add")
@@ -64,24 +73,26 @@ class Mapping(qtypes.Null):
         self.append(self.remove)
     
     def on_add(self):
-        get_qtypes_widget(self.key_type, "key", self, -2)
-        get_qtypes_widget(self.value_type, "value", self, -2)
+        item = qtypes.Null("item")
+        self.insert(-2, item)
+        get_qtypes_widget(self.key_type, "key", item, self.named_types)
+        get_qtypes_widget(self.value_type, "value", item, self.named_types)
 
     def on_remove(self):
         try:
-            self.removeChild(self.children.pop(-3))
             self.removeChild(self.children.pop(-3))
         except IndexError:
             pass  # Nothing to remove
 
 
 class Union(qtypes.Null):
-    def __init__(self, name, type_options, parent, at=None):
+    def __init__(self, name, type_options, parent, named_types, at=None):
         super().__init__(name)
         if at is None:
             parent.append(self)
         else:
             parent.insert(at, self)
+        self.named_types = named_types
         self.type_enum = qtypes.Enum("type", value={"allowed": type_options})
         self.type_enum.updated.connect(self.on_type_updated)
         self.append(self.type_enum)
@@ -91,21 +102,36 @@ class Union(qtypes.Null):
     def on_type_updated(self, value):
         self.takeChild(1)
         if value["value"] not in ("None", "NoneType"):
-            get_qtypes_widget(value["value"], "value", self)
+            get_qtypes_widget(value["value"], "value", self, self.named_types)
+
+
+class NamedTuple(qtypes.Null):
+    def __init__(self, name, type_map, parent, named_types, at=None):
+        super().__init__(name)
+        if at is None:
+            parent.append(self)
+        else:
+            parent.insert(at, self)
+        self.named_types = named_types
+        for k, v in type_map.items():
+            get_qtypes_widget(v, k, self, self.named_types)
+
 
 
 
 # TODO: default values
 # TODO: Tuples (Named Tuples)
-def get_parameter_widget(parameter, parent):
+def get_parameter_widget(parameter, parent, named_types):
     if "annotation" not in parameter or "type" not in parameter["annotation"]:
         print(f"No annotation for {parameter['name']}")
         ret = qtypes.Null(parameter["name"])
         parent.append(ret)
         return ret
-    return get_qtypes_widget(parameter["annotation"]["type"], parameter["name"], parent)
+    if parameter["kind"]["name"] == "VAR_POSITIONAL":
+        return Sequence(parameter["name"], parameter["annotation"]["type"], parent, named_types)
+    return get_qtypes_widget(parameter["annotation"]["type"], parameter["name"], parent, named_types)
 
-def get_qtypes_widget(type_, name, parent, at=None):
+def get_qtypes_widget(type_, name, parent, named_types, at=None):
     expr = ast.parse(type_).body[0].value
     if isinstance(expr, ast.Name):
         ret = None
@@ -117,6 +143,10 @@ def get_qtypes_widget(type_, name, parent, at=None):
             ret = qtypes.Bool(name)
         elif expr.id == "str":
             ret = qtypes.String(name)
+        elif expr.id in named_types and isinstance(named_types[expr.id], list):
+            ret = qtypes.Enum(name, value={"allowed": named_types[expr.id]})
+        elif expr.id in named_types and isinstance(named_types[expr.id], dict):
+            ret = NamedTuple(name, named_types[expr.id], parent, named_types, at)
         if ret is not None:
             if at is None:
                 parent.append(ret)
@@ -143,19 +173,19 @@ def get_qtypes_widget(type_, name, parent, at=None):
         class_ = ast.unparse(expr.value)
         params = expr.slice
         if class_ in ("typing.Sequence", "typing.List", "typing.Iterable", "list"):
-            return Sequence(name, ast.unparse(params), parent, at)
+            return Sequence(name, ast.unparse(params), parent, named_types, at)
         elif class_ in ("typing.Mapping", "typing.Dict", "dict"):
             kt, vt = params.elts
-            return Mapping(name, ast.unparse(kt), ast.unparse(vt), parent, at)
+            return Mapping(name, ast.unparse(kt), ast.unparse(vt), parent, named_types, at)
         elif class_ == "typing.Optional":
             if isinstance(params, ast.Name):
-                return Union(name, ["None", ast.unparse(params)], parent, at)
-            return Union(name, ["None", ast.unparse(params)], parent, at)
+                return Union(name, ["None", ast.unparse(params)], parent, named_types, at)
+            return Union(name, ["None", ast.unparse(params)], parent, named_types, at)
         elif class_ == "typing.Union":
             if not isinstance(params, ast.Tuple):
                 # Only one option in union, don't bother with type selector enum
-                return get_qtypes_item(ast.unparse(params), name, parent, at)
-            return Union(name, [ast.unparse(x) for x in params.elts], parent, at)
+                return get_qtypes_item(ast.unparse(params), name, parent, named_types, at)
+            return Union(name, [ast.unparse(x) for x in params.elts], parent, named_types, at)
 
     print(f"{type_} not yet handled")
 
@@ -169,11 +199,16 @@ class PlanWindow(QtWidgets.QMainWindow):
         self.plan_sel.updated.connect(self.on_plan_updated)
 
         self.tw.append(self.plan_sel)
+        self.plan_sel.updated.emit(self.plan_sel.get())
 
     def on_plan_updated(self, plan):
+        print()
+        from pprint import pprint
+        pprint(ser[plan["value"]])
         self.plan_sel.takeChildren()
         for parm in ser[plan["value"]]["parameters"]:
-            get_parameter_widget(parm, self.plan_sel)
+            get_parameter_widget(parm, self.plan_sel, ser[plan["value"]]["named_types"])
+        self.plan_sel.setExpanded(True)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
